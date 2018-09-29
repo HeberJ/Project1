903a818ac54370f96b0c417e926a806666300f2c
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-09-29 12:29:15 -0500

    changed location of script tag links, was preventing jquery animations from working properly.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,15 +2,15 @@
 
 // show/hide table/card on mobile
 $('#weekInfoBtn').click(function() {
-    $('.back').toggle(2000);
-    $('.front').toggle(2000);
-    $('#weekInfoBtn').toggle(2000);
-    $('#weekInfoBtn2').toggle(2000);
+    $('.back').toggle("slow");
+    $('.front').toggle("slow");
+    $('#weekInfoBtn').toggle("slow");
+    $('#weekInfoBtn2').toggle("slow");
 })
 
 $('#weekInfoBtn2').click(function() {
-    $('.back').toggle(2000);
-    $('.front').toggle(2000);
-    $('#weekInfoBtn2').toggle(2000);
-    $('#weekInfoBtn').toggle(2000);
+    $('.back').toggle("slow");
+    $('.front').toggle("slow");
+    $('#weekInfoBtn2').toggle("slow");
+    $('#weekInfoBtn').toggle("slow");
 });
