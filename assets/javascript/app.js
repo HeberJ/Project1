1b22f45d087c9f680a1a5b131b21a8480fe5036b
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-04 18:10:58 -0500

    jquery animation movied from app to home

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -104,21 +104,6 @@ $(document).ready(function() {
     // APIs
     //******************************************************************************************************************
 
-    //******************************************************************************************************************
-    // Animations
-    //******************************************************************************************************************
-    // show/hide table/card on mobile
-    $('#weekInfoBtn').click(function() {
-        $('.back').slideToggle('slow', 'linear');
-        $('.front').slideToggle('slow', 'linear');
-        $('#weekInfoBtn').toggle('slow', 'linear');
-        $('#weekInfoBtn2').toggle('slow', 'linear');
-    });
-
-    $('#weekInfoBtn2').click(function() {
-        $('.back').slideToggle('slow', 'linear');
-        $('.front').slideToggle('slow', 'linear');
-        $('#weekInfoBtn2').toggle('slow', 'linear');
-        $('#weekInfoBtn').toggle('slow', 'linear');
-    });
+    
+    
 });
--- assets/javascript/home.js
+++ assets/javascript/home.js
@@ -153,6 +153,24 @@ $(document).ready(function() {
       $('<iframe src="https://www.youtube.com/embed/n65HW1iJUuY?controls=0" frameborder="0" scrolling="no" id="myFrame"></iframe>')
       .appendTo('.wrapper');
 
+    //******************************************************************************************************************
+    // Animations
+    //******************************************************************************************************************
+      // show/hide table/card on mobile
+    $('#weekInfoBtn').click(function() {
+        $('.back').slideToggle('slow', 'linear');
+        $('.front').slideToggle('slow', 'linear');
+        $('#weekInfoBtn').toggle('slow', 'linear');
+        $('#weekInfoBtn2').toggle('slow', 'linear');
+    });
+
+    $('#weekInfoBtn2').click(function() {
+        $('.back').slideToggle('slow', 'linear');
+        $('.front').slideToggle('slow', 'linear');
+        $('#weekInfoBtn2').toggle('slow', 'linear');
+        $('#weekInfoBtn').toggle('slow', 'linear');
+    });
+
 });
 
 
